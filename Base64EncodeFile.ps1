8a524b043709844662fad980290da3f4682b2cc1
Ashutosh Rathore <ashutoshrathore.mail@gmail.com>
2022-07-19 17:19:01 +0530

    Update Base64EncodeFile.ps1

--- Base64EncodeFile.ps1
+++ Base64EncodeFile.ps1
@@ -1,2 +1,22 @@
 $filename = "C:\sample.key"
 [Convert]::ToBase64String([IO.File]::ReadAllBytes($filename)) | clip
+
+
+
+
+#Encoding and decoding of strings
+$ClientID = "ThisIsMyClientID"
+$ClientSecret = "ThisIsMyClientSecretKEY"
+
+$ClientIDEncoded = [Convert]::ToBase64String([Text.Encoding]::Unicode.GetBytes($ClientID))
+$ClientSecretEncoded = [Convert]::ToBase64String([Text.Encoding]::Unicode.GetBytes($ClientSecret))
+
+$ClientIDEncoded
+$ClientSecretEncoded
+
+
+$ClientIDDeoded = [System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String($ClientIDEncoded))
+$ClientSecretDecoded = [System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String($ClientSecretEncoded))
+
+$ClientIDDeoded
+$ClientSecretDecoded
